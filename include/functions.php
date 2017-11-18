8e70e2ca93c4104112cc9eaeef845cb6f0381cec
Jan Piskvor Martinec <github@piskvor.org>
2017-11-18 23:41:52 +0100

    convert names

--- include/functions.php
+++ include/functions.php
@@ -8,7 +8,7 @@ function getSanitizedName($displayId, $title, $fileName) {
 	if ($extPost !== false) {
 		$ext = substr($fileName, $extPost);
 	}
-	$convertedName = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $title);
+	$convertedName = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', trim($title));
 	$convertedName = preg_replace('/[^\w_-]/','_', $convertedName);
 	$convertedName = str_replace('_-_', '-',$convertedName);
 	$convertedName = preg_replace('/_+/','_', $convertedName);
