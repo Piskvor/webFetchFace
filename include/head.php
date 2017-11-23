3e224b597f7e5d021e7037eb2bfa0431af2436bb
Jan Piskvor Martinec <github@piskvor.org>
2017-11-23 14:00:50 +0100

    favicons

--- include/head.php
+++ include/head.php
@@ -2,6 +2,10 @@
 	<meta charset="utf-8">
 	<title>dl.piskvor.org</title>
 	<link rel="stylesheet" type="text/css" href="css/downloader.css" charset="utf-8" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="icon" type="image/png" href="images/favicon.png" sizes="16x16" />
+	<link rel="icon" type="image/png" href="images/favicon-192x192.png" sizes="192x192" />
+	<link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon-180x180.png" />
 	<script src="//code.jquery.com/jquery-latest.js" integrity="sha384-wciR07FV6RBcI+YEVsZy/bInxpyn0uefUp6Yi9R5r46Qv/yk/osR5nzY31koh9Uq" crossorigin="anonymous" type="text/javascript" charset="utf-8"></script>
 	<script>
 		if (typeof jQuery === 'undefined') {
