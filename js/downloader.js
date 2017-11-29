a0cfe2fd857fe2747660b4862802a05d5f607e5b
Jan Piskvor Martinec <github@piskvor.org>
2017-11-29 15:05:08 +0100

    touchend instead of dblclick

--- js/downloader.js
+++ js/downloader.js
@@ -47,14 +47,14 @@ $(document).ready(function () {
 		delay: 2000
 	});
 	$('.timeago').timeago();
-	$('.rowStatus').on('dblclick', function (dblClickEvt) {
+	$('.rowStatus').on('dblclick touchend', function (dblClickEvt) {
 		var $rs = $(dblClickEvt.target);
 		var md = $rs.data('metadatafilename');
 		if (md) {
 			window.open(md);
 		}
 	});
-	$('.rowDate').on('dblclick', function (dblClickEvt) {
+	$('.rowDate').on('dblclick touchend', function (dblClickEvt) {
 		var $rs = $(dblClickEvt.currentTarget);
 		var md = $rs.data('outfilename');
 
